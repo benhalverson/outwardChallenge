2b248580ac5829bedf91ba7980136f5082ecc52b
Ben Halverson <benhalverson33@gmail.com>
2018-09-17 10:45:03 -0700

    feat(tracking): tracking number of views on all routes

--- index.js
+++ index.js
@@ -6,84 +6,124 @@ const PORT = process.env.PORT || 3000;
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({'extended': true}));
 app.use(bodyParser.json());
-
+let totalVisit = 0;
 app.get('/', (req, res) => {
-  // count the number of visit on the root...
-})
+  totalVisit++;
+  res.json({
+    views: totalVisit
+  });
+});
 
 
 app.get('/math/add/:id/:id2', (req, res) => {
+  totalVisit++;
   const x = parseInt(req.params.id);
   const y = parseInt(req.params.id2);
   const data = [x, y].reduce((a, b) => a + b);
-  res.json({ data });
+  res.json({ 
+    data,
+    views: totalVisit
+   });
 });
 
 app.get('/math/subtract/:id/:id2', (req, res) => {
+  totalVisit++;
   const x = parseInt(req.params.id);
   const y = parseInt(req.params.id2);
   const data = [x, y].reduce((a, b) => a - b);
-  res.json({ data });
+  res.json({
+    data,
+    views: totalVisit
+  });
 });
 
 app.get('/math/multiply/:id/:id2', (req, res) => {
+  totalVisit++;
+
   const x = parseInt(req.params.id);
   const y = parseInt(req.params.id2);
   const data = [x, y].reduce((a, b) => a * b);
-  res.json({ data });
+  res.json({
+    data,
+    views: totalVisit
+  });
 });
 
 app.get('/math/multiply/:id/:id2', (req, res) => {
+  totalVisit++;
   const x = parseInt(req.params.id);
   const y = parseInt(req.params.id2);
   const data = [x, y].reduce((a, b) => a * b);
-  res.json({ data });
+  res.json({
+    data,
+    views: totalVisit
+  });
 });
 
 app.get('/math/divide/:id/:id2', (req, res) => {
+  totalVisit++;
   const x = parseInt(req.params.id);
   const y = parseInt(req.params.id2);
   const data = [x, y].reduce((a, b) => a / b);
-  res.json({ data });
+  res.json({
+    data,
+    views: totalVisit
+  });
 });
 
 app.post('/math', (req, res) => {
+  totalVisit++;
   if (!req.body.value1 || !req.body.value2 ) {
     return res.status(400).send({
       sucess: false,
-      message: 'value 1 and 2 are required'
+      message: 'value 1 and 2 are required',
+      views: totalVisit
     });
   }
   if (!req.body.operand) {
     return res.status(400).send({
       sucess: false,
-      message: 'operand is required'
+      message: 'operand is required',
+      views: totalVisit
     });
   }
   const total = {
     value1: parseFloat(req.body.value1),
     value2: parseFloat(req.body.value2),
-    operand: req.body.operand
+    operand: req.body.operand,
+    views: totalVisit
   }
 
   if (total.operand === '+') {
     const data = [total.value1, total.value2].reduce((a, b) => a + b); 
-    res.json({ data }); 
+    res.json({
+      data,
+      views: totalVisit
+    });
   }
 
   if (total.operand === '-') {
     const data = [total.value1, total.value2].reduce((a, b) => a - b); 
-    res.json({ data }); 
+    res.json({
+      data,
+      views: totalVisit
+    });
   }
 
   if (total.operand === '*') {
     const data = [total.value1, total.value2].reduce((a, b) => a * b); 
-    res.json({ data }); 
+    res.json({
+      data,
+      views: totalVisit
+    });
   }
 
   if (total.operand === '/') {
     const data = [total.value1, total.value2].reduce((a, b) => a / b); 
-    res.json({ data }); 
+    res.json({
+      data,
+      views: totalVisit
+    });
   }
 
 });
